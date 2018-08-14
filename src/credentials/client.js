87fe3505f3a67b21059dd276bcfaf4c7b9b9c40b
Kazuki Shibata <shibata@wanta.fun>
2018-08-15 01:14:25 +0900

    [rm] 削除

--- src/credentials/client.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyDk4CleI0mfzFSU7WDnOyzTVycybPZCA5Y",
-  authDomain: "picturebookmemo.firebaseapp.com",
-  databaseURL: "https://picturebookmemo.firebaseio.com",
-  projectId: "picturebookmemo",
-  storageBucket: "picturebookmemo.appspot.com",
-  messagingSenderId: "1009349291352"
-};
